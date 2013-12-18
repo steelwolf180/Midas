fb41cdc4bd3c667031ef5656cc266490f3cbf599
RichieStacker <richiestacker@yahoo.co.uk>
2013-12-18 15:38:45 +0000

    Made variable names more descriptive and brought them in line with Python's style guildlines, added comments, added formatting to show values to 2 decimal places, corrected some spelling errors.

--- Financial_Plan.py
+++ Financial_Plan.py
@@ -1,30 +1,38 @@
-Salary = float(input("Monthly Income :$"));
+salary = float(input("Monthly Income :$"));
 
-Current_Savings = float(Salary*0.1);
-Current_Spending = float(Salary*0.1);
-Current_Donation = float(Salary*0.1);
-Current_Learning = float(Salary*0.05);
+# Calculate expected outcome for the month.
+expected_savings = float(salary*0.1);
+expected_spending = float(salary*0.1);
+expected_donation = float(salary*0.1);
+expected_learning = float(salary*0.05);
 
+# Request input for any standing orders already in effect.
 print("\n=========Standing Order============\n");
-Input_Savings = float(input("Monthly Savings :$"));
-Input_Spending = float(input("Monthly Spending :$"));
-Input_Donation = float(input("Monthly Dondation :$"));
+standing_savings = float(input("Monthly Savings :$"));
+standing_spending = float(input("Monthly Spending :$"));
+standing_donation = float(input("Monthly Donation :$"));
 
-Savings = Input_Savings - Current_Savings;
-Spending = Input_Spending - Current_Spending;
-Donation = Input_Donation - Current_Donation;
-Expanses =(Salary - Input_Savings -  Input_Spending -  Input_Donation - Current_Learning);
+# Calculate shortfall (any outcome that remains to be spent)
+shortfall_savings = standing_savings - expected_savings;
+shortfall_spending = standing_spending - expected_spending;
+shortfall_donation = standing_donation - expected_donation;
+expenses =(salary - standing_savings -  standing_spending -  standing_donation - expected_learning);
 
+# Display outcome so far.
 print("\n====Income Distributed To Account=====\n");
-print("Calculated Savings:$"+str(round(Input_Savings, 2)));
-print("Calculated Spending:$"+str(round(Input_Spending, 2)));
-print("Calculated Donation:$"+str(round(Input_Donation, 2)));
-print("Calculated Learning:$"+str(round(Current_Learning, 2)));
+print("Calculated Savings:$" + "%.2f" % round(standing_savings, 2));
+print("Calculated Spending:$" + "%.2f" % round(standing_spending, 2));
+print("Calculated Donation:$" + "%.2f" % round(standing_donation, 2));
+print("Calculated Learning:$" + "%.2f" % round(expected_learning, 2));
+
+# Display remaining shortfall.
 print("\n=============Shortfall==============\n");
-print("Savings:$"+str(round(Savings, 2)));
-print("Spending:$"+str(round(Spending, 2)));
-print("Donation:$"+str(round(Donation, 2)));
-print("Learning:$"+str(round(Current_Learning, 2)));
-print("\n======Salary Left For Expanses======\n");
-print("Personal Expanses:$"+str(round(Expanses, 2)));
-print("Monthly Salary:$"+str(round(Salary, 2))+"\n");
+print("Savings:$" + "%.2f" % round(shortfall_savings, 2));
+print("Spending:$" + "%.2f" % round(shortfall_spending, 2));
+print("Donation:$" + "%.2f" % round(shortfall_donation, 2));
+print("Learning:$" + "%.2f" % round(expected_learning, 2));
+
+# Display remaining money left.
+print("\n======Salary Left For Expenses======\n");
+print("Personal Expenses:$" + "%.2f" % round(expenses, 2));
+print("Monthly Salary:$" + "%.2f" % round(salary, 2) + "\n");
