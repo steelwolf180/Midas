ba11d2238c4cb9afbb8351dd6c5fa52f2e95dd9b
steelwolf180 <steelwolf180@gmail.com>
2013-12-25 00:24:55 +0800

    Adding exit program function

--- Financial_Plan.py
+++ Financial_Plan.py
@@ -38,3 +38,6 @@ print("Learning:$" + "%.2f" % round(expected_learning, 2));
 print("\n======Salary Left For Expenses======\n");
 print("Personal Expenses:$" + "%.2f" % round(expenses, 2));
 print("Monthly Salary:$" + "%.2f" % round(salary, 2) + "\n");
+
+# To exit the python program
+input("Please press enter to exit the program")
